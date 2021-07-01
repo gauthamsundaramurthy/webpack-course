56a0796beba7448239c4f956a659f3c0d74b2add
Gautham <gautham.oct3@gmail.com>
2021-07-01 12:52:50 +0530

    using env option

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,111 +4,113 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = {
-    mode: 'production',
-    entry: {
-        main: ["@babel/polyfill", "./src/main.js"]
-    },
-    output: {
-        filename: "[name]-bundle.js",
-        path: path.resolve(__dirname, "../dist"),
-        publicPath: "/"
-    },
-    devServer: {
-        stats: {
-            colors: true
+module.exports = env => {
+    return {
+        mode: 'production',
+        entry: {
+            main: ["@babel/polyfill", "./src/main.js"]
         },
-        writeToDisk: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
+        output: {
+            filename: "[name]-bundle.js",
+            path: path.resolve(__dirname, "../dist"),
+            publicPath: "/"
+        },
+        devServer: {
+            stats: {
+                colors: true
             },
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: "babel-loader"
-                    }
-                ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: {
-                                localIdentName: "[path][name]__[local]--[hash:base64:5]"
+            writeToDisk: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: "html-loader"
+                        }
+                    ]
+                },
+                {
+                    test: /\.js$/,
+                    use: [
+                        {
+                            loader: "babel-loader"
+                        }
+                    ],
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: "css-loader",
+                            options: {
+                                modules: {
+                                    localIdentName: "[path][name]__[local]--[hash:base64:5]"
+                                }
                             }
                         }
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        "postcss-preset-env"
-                                    ]
-                                ],
-                            },
+                    ]
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: "style-loader"
+                        },
+                        {
+                            loader: "css-loader"
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                postcssOptions: {
+                                    plugins: [
+                                        [
+                                            "postcss-preset-env"
+                                        ]
+                                    ],
+                                },
+                            }
+                        },
+                        {
+                            loader: "sass-loader"
                         }
-                    },
-                    {
-                        loader: "sass-loader"
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name]-[contenthash].css"
-        }),
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify("production")
-            }
-        })
-    ],
-    optimization: {
-        minimizer: [
-          // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-          // `...`,
-          new CssMinimizerPlugin(),
+                    ]
+                },
+                {
+                    test: /\.(png|jpe?g|gif)$/i,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "[name]-[contenthash].css"
+            }),
+            new HtmlWebpackPlugin({
+                template: "./src/index.html"
+            }),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify(env.NODE_ENV)
+                }
+            })
         ],
-      },
+        optimization: {
+            minimizer: [
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+            // `...`,
+            new CssMinimizerPlugin(),
+            ],
+        }
+    }
 }
